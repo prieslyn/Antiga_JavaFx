daabab0595398a2067f12e4918a3a85a9a8fc4bd
prieslyn <200903391+prieslyn@users.noreply.github.com>
2025-09-20 13:05:25 +0800

    Added JavaFx Login

---
+++ src/antiga_javafx/Antiga_JavaFx.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package antiga_javafx;
+
+/**
+ *
+ * @author U S E R
+ */
+public class Antiga_JavaFx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
